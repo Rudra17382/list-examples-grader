a15d7118fa1af6077a6f6307307300c2ed3d37ff
Rudra17382 <rudrarupani@yahoo.com>
2022-11-28 11:09:17 -0800

    a

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,10 +1,65 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
 
 public class TestListExamples {
-  @Test(timeout = 100)
-  public void testTimeout() {
-    while(true) {}
-  }
   // Write your grading tests here!
+  @Test
+  public void filterCorrectly() {
+    List<String> i = new ArrayList<String>();
+    List<String> endList = new ArrayList<String>();
+    i.add("1Hello");
+    i.add("We");
+    i.add("1Are");
+    i.add("Here");
+    StringChecker wc = new ObjChecker();
+    ListExamples.filter(i, wc);
+    endList.add("1Hello");
+    endList.add("1Are");
+    assertEquals(endList,ListExamples.filter(i,wc));
+  }
+
+    @Test
+    public void correctOrderTest(){
+      List<String> list1 = new ArrayList<String>();
+      List<String> list2 = new ArrayList<String>();
+      list1.add("brownies");
+      list1.add("cakes");
+      list1.add("cookies");
+      list2.add("broccoli");
+      list2.add("carrots");
+      list2.add("spinach");
+      List<String> list3 = ListExamples.merge(list1, list2);
+      String[] expected = {"broccoli", "brownies","cakes","carrots","cookies","spinach"};
+      assertArrayEquals(expected, list3.toArray());
+  }
+
+  @Test
+    public void correctOrderTest2(){
+        List<String> list1 = new ArrayList<String>();
+        List<String> list2 = new ArrayList<String>();
+        list1.add("brownies");
+        list1.add("cakes");
+        list1.add("cookies");      //duplicate entry
+        list2.add("broccoli");
+        list2.add("carrots");
+        list2.add("cookies");     //duplicate entry
+        list2.add("spinach");
+        List<String> list3 = ListExamples.merge(list1, list2);
+        String[] expected = {"broccoli", "brownies", "cakes", "carrots", "cookies", "cookies", "spinach"};
+        assertArrayEquals(expected, list3.toArray());
+    }
+}
+
+class ObjChecker implements StringChecker{
+
+  public boolean checkString(String s){
+    if(Character.isDigit(s.charAt(0))){
+      return true;
+    }else{
+    return false;
+    }  
+  }
 }
--- grade.sh
+++ grade.sh
@@ -1,9 +1,54 @@
-# Create your grading script here
+rm -rf $"student_dir"
+git clone $1 $"student_dir" || exit 1 
 
-rm -rf student-submission
-git clone $1 student-submission
-echo 'Finished cloning'
+# Total student score out of 35
+score=0
 
+function display_score(){
 
+	printf "Student is given a score of $1/35 \n"
+	exit 0
+}
 
+directory_for_junit=$PWD
+classPath=".:$directory_for_junit/lib/hamcrest-core-1.3.jar:$directory_for_junit/lib/junit-4.13.2.jar"
 
+set -o pipefail
+
+cd ./$"student_dir"/
+
+cp $directory_for_junit/"TestListExamples".java ./
+
+printf "<<-- Starting Compilation -->>\n"
+
+javac -cp $classPath *.java | tee ../output1.txt
+
+if [[ $? -ne 0 ]]; then
+	
+	echo The student code did not compile.
+	display_score $score
+
+fi
+
+score=$((score + 5))
+
+printf "<<-- Running Java -->>\n" 
+
+java -cp $classPath org.junit.runner.JUnitCore "TestListExamples" | tee ../output2.txt
+
+if grep "OK" ../output2.txt; then
+    score=$((score + 30))
+    echo Student passed all tests!
+elif grep -q -E -w "Tests run:|Failures:" ../output2.txt; then
+    result=$(grep -E -w "Tests run:|Failures:" ../output2.txt)
+    tests=$(echo $result | grep -Eo '[0-9]{1,10}')
+
+    tests_run=$(echo $tests | cut -d' ' -f1) 
+    fail=$(echo $tests | cut -d' ' -f2)
+    pass=$((tests_run-fail))
+
+    score=$((score + 30*pass/tests_run)) 
+    echo Student passed $pass tests out of $tests_run tests
+fi
+
+display_score $score
